a53c54f2f37c3c8465682fddfce029c24341e4b2
Fabiana Banegas <fabianabanegasrivero@gmail.com>
2026-01-09 22:16:55 +0000

    supabase connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';  // Install: npm i @astrojs/node
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'server',
+  adapter: node(),
+  vite: { plugins: [tailwindcss()] }
 });
